fe77dd21ca6b024912d4ad1b1a8c4a21130b1507
Shayan Mohsin <shaymotionfilms@gmail.com>
2021-03-05 14:42:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => tutorials.map(tutorial => tutorial.split(" ").map(
-   word => word[0].toUpperCase() + word.slice(1)
-).join(' ')
+const titleCased = () => {
+  return tutorials.map(tutorial => {
+    return tutorial.split(' ').map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(' ');
+  });
+}
